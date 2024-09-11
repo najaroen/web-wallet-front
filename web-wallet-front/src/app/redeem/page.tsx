df82d5465f1999e0ae8091f683710b7b51a1663e
Nacharoen <najaroen_tik@live.com>
2024-09-11 11:26:37 +0700

    feature: add scan qr

--- web-wallet-front/src/app/redeem/page.tsx
+++ web-wallet-front/src/app/redeem/page.tsx
@@ -1,6 +1,14 @@
+"use client";
 import Image from "next/image";
+import Popup from "reactjs-popup";
+import { Scanner } from "@yudiel/react-qr-scanner";
 import scan from "@/public/assets/icons/90337376516257455273620.svg";
+import { useState } from "react";
+import React from "react";
 export default function Redeeming() {
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
+
   return (
     <div
       className="flex flex-col justify-between h-screen gap-10 p-0 sm:p-20 font-[family-name:var(--font-geist-sans)]"
@@ -12,25 +20,44 @@ export default function Redeeming() {
         <p className="text-center text-2xl font-semibold">Scan to redeem</p>
         <p className="text-center">SASOM Point</p>
       </div>
-      <div className="self-center">
-        <div
-          className="w-72 h-64 bg-white rounded-rd16 shadow-lg pt-16"
-          style={{ borderRadius: "16px" }}
+      <div>
+        <Popup
+          open={open}
+          closeOnDocumentClick
+          onClose={closeModal}
+          position={"bottom center"}
         >
-          <Image
-            style={{ marginLeft: "33%" }}
-            className=""
-            src={scan}
-            alt="Next.js logo"
-            width={100}
-            height={40}
-            priority
-          />
-          <p style={{ marginLeft: "40%" }} className="text-2xl font-bold">
-            scan
-          </p>
-        </div>
+          <div className="modal" style={{ height: "450px" }}>
+            <Scanner
+              onScan={(result) => {
+                console.log(result);
+              }}
+            />
+          </div>
+        </Popup>
       </div>
+      {!open ? (
+        <div className="self-center">
+          <div
+            onClick={() => setOpen((o) => !o)}
+            className="w-72 h-64 bg-white rounded-rd16 shadow-lg pt-16"
+            style={{ borderRadius: "16px" }}
+          >
+            <Image
+              style={{ marginLeft: "33%" }}
+              className=""
+              src={scan}
+              alt="Next.js logo"
+              width={100}
+              height={40}
+              priority
+            />
+            <p style={{ marginLeft: "40%" }} className="text-2xl font-bold">
+              scan
+            </p>
+          </div>
+        </div>
+      ) : null}
       <div className="px-4 text-sm text-center">
         <p>Redeem your point by X</p>
         <p>Contact us at: Sasompoint@sasom.com</p>
